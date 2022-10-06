f24c1235da043008a2f395a2a49fd34b78b81847
Jay Wang <jay.wang@hp.com>
2022-10-06 14:38:15 +0800

    first commit

---
+++ AwsSqsService.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.Extensions.Hosting;
+
+public class AwsSqsService : BackgroundService
+{
+    private string _queueUrl;
+
+    public AwsSqsService()
+    {
+        _queueUrl = "https://sqs.us-west-2.amazonaws.com/22021*****12/SVTPWebQueue";
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var amazonSqsClient = new AmazonSQSClient();
+        do
+        {
+            var receiveMessageResponse = await amazonSqsClient.ReceiveMessageAsync(new ReceiveMessageRequest
+            {
+                QueueUrl = _queueUrl,
+                WaitTimeSeconds = 20
+            });
+            if (receiveMessageResponse.Messages.Count != 0)
+            {
+                var message = receiveMessageResponse.Messages.FirstOrDefault();
+                Console.WriteLine(message.Body);
+                await amazonSqsClient.DeleteMessageAsync(new DeleteMessageRequest
+                {
+                    QueueUrl = _queueUrl,
+                    ReceiptHandle = message.ReceiptHandle
+                });
+            }
+        } while (true);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var hostBuilder = new HostBuilder().ConfigureServices(services => services.AddHostedService<AwsSqsService>());
+var app = hostBuilder.Build();
+app.Run();
